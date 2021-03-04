f9d76b67c8931c518a254f4ee283b42501dd7be7
Rick <rickyang2910@gmail.com>
2021-03-04 17:36:30 +0800

    Init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Row, Col, Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="success" className="mr-1">
+        Success
+      </Button>
+    </div>
+  );
+}
+
+export default App;
